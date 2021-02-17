736a0ccab6df191a9b518478315788e28e830fe2
Fabien <depasse.fabien@gmail.com>
2021-02-17 18:06:11 +0000

    starter files

---
+++ client/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+const env = process.env.NODE_ENV === 'production' ? (
+  new webpack.EnvironmentPlugin({ ...process.env })
+) : (
+  new Dotenv()
+)
+
+module.exports = () => {
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve('dist'),
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+        { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        { test: /\.s(a|c)ss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+        { test: /\.(png|jpe?g|gif|svg)$/i, use: 'file-loader' }
+      ]
+    },
+    devServer: {
+      contentBase: path.resolve('src'),
+      hot: true,
+      open: true,
+      port: 8001,
+      watchContentBase: true,
+      historyApiFallback: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8000',
+          secure: false
+        }
+      }
+    },
+    plugins: [
+      new Dotenv(),
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+      }),
+      env
+    ]
+  }
+}
