d1c0c3a8bf54da251875740ae47c4996a1652efd
Kate Joyce <katejoyce156@googlemail.com>
2021-02-21 18:12:15 +0000

    conflicts

--- __tests__/helper.js
+++ __tests__/helper.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-//!This helper file will run first and will run on the command - NPM RUN TEST
-process.env.NODE_ENV = 'test'
-
-//! Chai - assertion
-//! Assertions evaluate to true or false, checking if the values match expected values...(is recipe a number a string?)
-import chai from 'chai'
-
-//! make it a global variable
-global.expect = chai.expect
-
-//! SuperTest - this can let you make requests programmatically 
-import supertest from 'supertest'
-import expressApp from '../index.js'
-
-//!! this is wrapping the entire express App and then wrapping it so we can run tests on the program
-global.api = supertest(expressApp)
-=======
 // Setting environemnt to be test on npm run test
 process.env.NODE_ENV = 'test'
 
@@ -27,4 +9,3 @@ global.expect = chai.expect
 import supertest from 'supertest'
 import expressApp from '../index.js'
 global.api = supertest(expressApp)
->>>>>>> development
