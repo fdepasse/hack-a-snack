5e46e7ad8bc8238363ec25f74933979282877f7b
Kate Joyce <katejoyce156@googlemail.com>
2021-02-23 11:10:25 +0000

    link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -22,11 +22,9 @@ const Navbar = ({ history }) => {
 
   return <nav className="navbar" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
-      <a className="navbar-items">
-        <Link to={'/'}>
-          <img src="https://cdn.shopify.com/s/files/1/0306/1016/1723/files/logo.png?v=1614074360" width={220}/>
-        </Link>
-      </a>
+      <Link className="navbar-items" to={'/'}>
+        <img src="https://cdn.shopify.com/s/files/1/0306/1016/1723/files/logo.png?v=1614074360" width={220} />
+      </Link>
       <a onClick={() => updateMobNav(!mobNav)} role="button" className={`navbar-burger ${mobNav ? 'is-active' : ''}`} >
         <span aria-hidden="true" />
         <span aria-hidden="true" />
